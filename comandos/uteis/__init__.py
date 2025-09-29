6dd48275a09f2946751d75e55189c0ff4eafd401
Ericson Barros <ericsonengwork@gmail.com>
2025-09-28 21:05:56 -0300

    alteração feita -28-09-2025,21:00

--- comandos/uteis/__init__.py
+++ comandos/uteis/__init__.py
@@ -12,4 +12,13 @@ def piso(area,rendimento):
     resp = ((area + (area * 15 / 100)) / rendimento)
     return f'{resp:.2f}'.replace('.', ',')
 
+def areaRevestimmento():
+    print('Vamos calcular a area do revestimento, por favor informe a largura e comprimento(em metros) da peça que sera utilizada:')
+    l = float(input('Largura: '))
+    c = float(input('Comprimento: '))
+    resp = l * c
+    return f'A a area do revestimento é {resp:.2f}m²'.replace('.', ',')
+    print(linha())
+
+
 
